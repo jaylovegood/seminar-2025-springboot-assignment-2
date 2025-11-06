03cf06d346c1fdd3dbb0a6398312c07bb5ae2a52
jaylovegood <jaylovegood@snu.ac.kr>
2025-11-07 00:37:01 +0900

    #19 lecture 및 lecture schedule 관련 db migration 파일 수정

--- src/main/resources/db/migration/V7__create_table_lectures.sql
+++ src/main/resources/db/migration/V7__create_table_lectures.sql
@@ -1,5 +1,18 @@
 CREATE TABLE IF NOT EXISTS lectures
 (
     id         BIGINT AUTO_INCREMENT
-        PRIMARY KEY
+        PRIMARY KEY,
+    academic_year INT NOT NULL,
+    semester VARCHAR(10) NOT NULL,
+    lecture_type VARCHAR(10) NOT NULL,
+    college VARCHAR(30) NOT NULL,
+    department VARCHAR(30) NOT NULL,
+    target VARCHAR(20) NOT NULL,
+    grade INT NOT NULL,
+    course_number VARCHAR(20) NOT NULL,
+    lecture_number VARCHAR(20) NOT NULL,
+    title VARCHAR(50) NOT NULL,
+    subtitle VARCHAR(50) NOT NULL,
+    credit INT NOT NULL,
+    lecturer VARCHAR(50) NOT NULL
 );
